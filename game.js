a5b4118403b88aaddf643cdeab1f0571bed0cdf5
Çağın Yarkın Sarı <cyarkinsari@gmail.com>
2022-08-24 14:18:01 +0300

    answer control

--- game.js
+++ game.js
@@ -1,28 +1,41 @@
-// Initial Arrays
+// Initial Arrays & Variables
 var buttonColors = ["red","blue","green","yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
-
+var level = 0;
 // Random Generated Color
 function nextSequence (){
     var randomNumber = Math.floor((Math.random()*4));
     var randomChosenColor = buttonColors[randomNumber];
     playSound(randomChosenColor);
-    return randomChosenColor
+
+    // Saving Pattern
+    gamePattern.push(randomChosenColor);
+
+    // Flash Animation for sequence
+    $("#"+randomChosenColor).animate({opacity: 0}, 150).animate({opacity: 100}, 150);
+
+    // Level  
+    level++;
+    $("h1").html("Level "+level);
 };
-
-// Saving Pattern
-gamePattern.push(nextSequence());
-
-// Flash Animation for sequence
-$("#"+nextSequence()).animate({opacity: 0}, 150).animate({opacity: 100}, 150)
+// Answer Control
+function checkAnswer (currentLevel){
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+        console.log("success");
+    }else{
+        console.log("wrong")
+    }
+    
+};
 
 // Click Handler
 $(".btn").on("click", (event)=>{
     var userChoosenColor = event.target.id;
     playSound(userChoosenColor);
     animatePress(userChoosenColor);
-    userClickedPattern.push(userChoosenColor);   
+    userClickedPattern.push(userChoosenColor);
+    checkAnswer((userClickedPattern.length-1));   
 });
 
 // Flash Animation for Clicks
@@ -40,6 +53,14 @@ function playSound(name){
 };
 
 // Any Button to Start Game
-$(document).on("keypressed", (event)=>{
-
+var count = 0;
+$(document).on("keypress", (event)=>{   
+    if (count === 0){
+        nextSequence();
+        count++;
+    }
+    
 });
+
+
+// if game over count = 0
